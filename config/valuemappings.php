6494de0653d867d1496a2219a8ecda02abc2c48f
alex <karpen_ko@hotmail.com>
2019-03-27 18:03:57 +0100

    Extended valuemappings config - added Studiengänge to config/valuemappings config - added Incoming (mit Agreement) to config/valuemappings config

--- config/valuemappings.php
+++ config/valuemappings.php
@@ -53,27 +53,51 @@ $config['valuemappings']['frommo']['anrede'] = array(
 );
 
 $config['valuemappings']['frommo']['studiengang_kz'] = array(
+	'Biomedical Engineering' => 227,
 	'Elektronik' => 254,
-	'Wirtschaftsinformatik' => 256,
-	'Wirtschaftsinformatik Master' => 302,
+	'Elektronik/Wirtschaft' => 255,
+	'Embedded Systems' => 297,
+	'Erneuerbare Urbane Energiesysteme' => 578,
+	'Game Engineering und Simulation' => 585,
+	'Gesundheits- und Rehabilitationstechnik' => 329,
+	'Industrielle Elektronik' => 300,
+	'Informatik/Computer Science' => 257,
+	'Informations- und Kommunikationssysteme' => 258,
+	//'Informationsmanagement und Computersicherheit' => 0,
+	'Innovations- und Technologiemanagement' => 301,
 	'Integrative Stadtentwicklung-Smart City' => 334,
 	'Internationales Wirtschaftsingenieurwesen' => 335,
-	'Informatik/Computer Science' => 257,
-	'Maschinenbau' => 804,
-	'Elektronik/Wirtschaft' => 255
+	'Internationales Wirtschaftsingenieurwesen Master' => 336,
+	'Maschinenbau' => 779,
+	'Maschinenbau Master' => 804,
+	'Mechatronik/Robotik' => 330,
+	'Mechatronik/Robotik Master' => 331,
+	//'Medical Engineering & eHealth' => 0,
+	'Smart Homes und Assistive Technologien' => 768,
+	'Softwareentwicklung' => 299,
+	'Sports Equipment Technology' => 327,
+	'Sports Equipment Technology Master' => 328,
+	'Technisches Umweltmanagement und Ökotoxikologie' => 332,
+	'Telekommunikation und Internettechnologien' => 298,
+	'Tissue Engineering and Regenerative Medicine' => 692,
+	'Unbekannt' => 0,
+	'Urbane Erneuerbare Energietechnologien' => 476,
+	'Verkehr und Umwelt' => 333,
+	'Wirtschaftsinformatik' => 256,
+	'Wirtschaftsinformatik Master' => 302
 );
 
 $config['valuemappings']['frommo']['zgvnation'] = $nations;
 
 $config['valuemappings']['frommo']['zgvmas_code'] = array(
-	'postsek.Ausland' => 4,
-	'postsek.Inland' => 3,
-	'FH-Bachelor (A)' => 2,
 	'FH-Bachelor (I)' => 1,
+	'FH-Bachelor (A)' => 2,
+	'postsek.Inland' => 3,
+	'postsek.Ausland' => 4,
+	'Uni-Bachelor (I)' => 5,
+	'Uni-Bachelor (A)' => 6,
 	'FH (I)' => 7,
 	'FH (A)' => 8,
-	'Uni-Bachelor (A)' => 6,
-	'Uni-Bachelor (I)' => 5,
 	'Uni (I)' => 9,
 	'Uni (A)' => 10
 );
@@ -84,4 +108,5 @@ $config['valuemappings']['frommo']['mobilitaetsprogramm_code'] = array(
 	'Erasmus SMS' => '7',
 	'Erasmus SMP' => '7',
 	'Erasmus Mundus' => '7',
+	'Incoming (mit Agreement)' => '13'
 );
